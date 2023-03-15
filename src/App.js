48604813f06e6e1501e69d1a67adb72499cb4867
fleite174 <andretsilva2509@gmail.com>
2023-03-15 01:17:27 -0300

    Dezin balaa fio esquece

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edite O <code>src/App.js</code> e salve para recarregar
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dj V1no é um esculacho
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
