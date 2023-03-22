008516dceac74f9a0bacb9f79166e084dfd83680
fleite174 <andretsilva2509@gmail.com>
2023-03-22 18:49:24 -0300

    Teste de modificacoes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite O <code>src/App.js</code> e salve para recarregar
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dj V1no é um esculacho
-        </a>
-      </header>
-    </div>
+    <a href='#to-do' className='acao'>Sobre</a>
   );
 }
 
